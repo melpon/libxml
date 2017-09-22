fd91eecbcdb2d8b9b771882c98d52b68e1d3c922
melpon <shigemasa7watanabe+github@gmail.com>
2017-09-22 20:28:00 +0900

    version up to 1.0.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Libxml.Mixfile do
     [
       app: :libxml,
       compilers: [:make_libxml] ++ Mix.compilers,
-      version: "1.0.0",
+      version: "1.0.3",
       elixir: "~> 1.4",
       description: "Thin wrapper for Libxml2 using NIF",
       package: [maintainers: ["melpon"],
