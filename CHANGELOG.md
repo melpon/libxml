4daf8e6f4782a6d9a98471cb8922ba41de539a0e
melpon <shigemasa7watanabe+github@gmail.com>
2018-01-27 20:39:43 +0900

    bump version to 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.1.0
+
+- Implement XML Schema functions
+
 ## 1.0.4
 
 - Add changelog
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Libxml.Mixfile do
     [
       app: :libxml,
       compilers: [:make_libxml] ++ Mix.compilers(),
-      version: "1.0.4",
+      version: "1.1.0",
       elixir: "~> 1.4",
       description: "Thin wrapper for Libxml2 using NIF",
       package: [
